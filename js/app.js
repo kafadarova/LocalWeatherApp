f920f7c0733f7eaf9267b9fcd6ab5eab599c4e1c
Kristin Kafadarova <k.kafadarova@gmail.com>
2017-12-03 16:03:35 +0100

    Styling added

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
    var country = data.country;
 
 //Locate the city
-   $("#location").html(city + ", " + country) ;
+   $("#location").html(city + "," + country) ;
 
    // Dark Sky API
    var url= "https://api.darksky.net/forecast/c1c79c93374cb0e0b5e2439d84fd12f5/";
@@ -43,6 +43,14 @@ function getWeather(WeatherAPI) {
    });
 
    function displayWeather(data){
+    var icon = data.currently.icon;
+    var skycons = new Skycons({"color": "white"});
+      // on Android, a nasty hack is needed: {"resizeClear": true}
+
+      // you can add a canvas by the canvas DOM element itself.
+      skycons.add(document.getElementById("animated-icon"), icon);
+      // start animation!
+      skycons.play();
      var temp = Math.round(data.currently.temperature);
      var tempC = Math.round((temp - 32) * 5/9);
      $("#temperature").text(temp + "°");
@@ -60,5 +68,22 @@ function getWeather(WeatherAPI) {
      $("#winds").text("Windspeed: " + data.currently.windSpeed);
      $("#pressure").text("Pressure: " + data.currently.pressure);
      $("#humidity").text("Humidity: " + data.currently.humidity);
+     };
    }
- };
+
+ $(".btn-search").click(function(){
+ var city_name = document.getElementsByClassName('user-location').value;
+ const API_KEY = "a2ad63e0576647360495140bd811409e";
+ var url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`;
+
+             $.getJSON(url, function (data) {
+            var rawJson = JSON.stringify(data);
+            var json = JSON.parse(rawJson);
+        });
+
+
+
+
+ $("#location").html(city + ", " + country) ;
+
+ })
